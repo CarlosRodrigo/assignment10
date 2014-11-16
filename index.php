b3881f5d1bbd150b040a290ee6ed2eb57633e4de
colin luther <cluther@uvm.edu>
2014-11-16 13:57:19 -0500

    add links to schema and ER diagram

--- index.php
+++ index.php
@@ -18,6 +18,8 @@
                 <li><a href="timesheet/home.php">Home Page.</a></li>
                 <li><a href="docs/documentspecifications.docx">Document Specifications.</a></li>
                 <li><a href="https://github.com/cluth11/assignment10">Project Repository.</a></li>
+                <li><a href="docs/TimeSheet.sql">Schema.</a></li>
+                <li><a href="docs/TimeSheet_ER_Diagram.png">Entity Relationship Diagram.</a></li>
                                
             </ul>            
         </nav>
