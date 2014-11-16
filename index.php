2efaa76cd57ba2c7f3df8425a98b9dd62d3bd862
colin luther <cluther@uvm.edu>
2014-11-16 13:45:23 -0500

    Adding index and work on doc specs

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>World Wide Web Design - Main Index</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Colin Luther, Carlos Garcia" />
+        <meta name="description" content="This is a simple index page for my CS148 Assignments." />
+
+    </head>
+
+    <body id="Index">
+        <header>
+            <h1>CS 148 Database Design for the Web: Final Project</h1>
+        </header>
+        <nav>
+            <ul>
+                
+                <li><a href="timesheet/home.php">Home Page.</a></li>
+                <li><a href="docs/documentspecifications.docx">Document Specifications.</a></li>
+                <li><a href="https://github.com/cluth11/assignment10">Project Repository.</a></li>
+                               
+            </ul>            
+        </nav>
+    </body>
+</html>
