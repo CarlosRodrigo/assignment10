fd98037fad477e628175b5ff04f81cdadfa0e133
CarlosRodrigo <carlosrodrigocrcg@gmail.com>
2014-11-19 22:28:59 -0500

    Project and User insert to database functionality

--- timesheet/project.php
+++ timesheet/project.php
@@ -2,6 +2,12 @@
 include("top.php");
 //include("header.php"); 
 include("nav.php");
+include("lib/functions.php");
+
+if(!isset($_SESSION['userID'])) {
+    header('location: login.php');
+    exit();
+}
 
 if (isset($_POST["btnSubmit"])) {
 
@@ -19,11 +25,20 @@ if (isset($_POST["btnSubmit"])) {
     $description = htmlentities($_POST["txtDescription"], ENT_QUOTES, "UTF-8");
     $dataRecord[] = $description;
 
-    $query = 'INSERT INTO tblProject SET fldName = ?, fldBudget = ?, fldExpectedHours = ?, fldDescription = ?';
+    if ($projectName == "") {
+        $emailERROR = true;
+        alert_danger("You were not able to add a project.");
+    } else {
+        $query = 'INSERT INTO tblProject SET fldName = ?, fldBudget = ?, fldExpectedHours = ?, fldDescription = ?';
 
-    $results = $thisDatabase->insert($query, $dataRecord);
-    
-    print_r($results);
+        $results = $thisDatabase->insert($query, $dataRecord);
+
+        if($results == true) {
+            alert_success("You've successfully add a project.");
+        } else {
+            alert_danger("You were not able to add a project.");
+        }
+    }
 }
 
 ?>
@@ -34,36 +49,61 @@ if (isset($_POST["btnSubmit"])) {
             <div class="box">
                 <div class="col-lg-12">
                     <hr>
-                    <h2 class="intro-text text-center">Add <strong>Project</strong></h2>
+                    <h2 class="intro-text text-center"><strong>Project</strong></h2>
                     <hr>
-                    <form action="<?php print $phpSelf; ?>" method="post">
-                        <div class="row">
-                            <div class="form-group col-lg-4">
-                                <label>Project Name</label>
-                                <input type="text" name="txtProjectName" class="form-control">
-                            </div>
-                            <div class="form-group col-lg-4">
-                                <label>Budget</label>
-                                <input type="text" name="txtBudget" class="form-control">
-                            </div>
-                            <div class="form-group col-lg-4">
-                                <label>Expected Hours</label>
-                                <input type="text" name="txtExpectedHours" class="form-control">
-                            </div>
-                            <div class="form-group col-lg-4">
-                                <label>Description</label>
-                                <textarea name="txtDescription" class="form-control" rows="6"></textarea>
-                            </div>
-                            <div class="clearfix"></div>
-                            <div class="form-group col-lg-12">
-                                <button type="submit" id="btnSubmit" name="btnSubmit" class="btn btn-default">Add</button>
+                    <div class="row">
+                        <div class="form-group col-lg-4">
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                                Add Project
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <ul class="list-group">
+                            <?php build_list_from_database($thisDatabase, 'SELECT fldName FROM tblProject ORDER BY fldName');?>
+                        </ul>
+                    </div>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                                    <h4 class="modal-title" id="exampleModalLabel">Add Project</h4>
+                                </div>
+                                <form role="form" method="post" action="<?php print $phpSelf; ?>">
+                                <div class="modal-body">
+                                    <div class="form-group">
+                                        <label>Project Name*</label>
+                                        <input type="text" name="txtProjectName" class="form-control" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Budget*</label>
+                                        <input type="text" name="txtBudget" class="form-control" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Expected Hours*</label>
+                                        <input type="text" name="txtExpectedHours" class="form-control" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Description*</label>
+                                        <textarea name="txtDescription" class="form-control" rows="6" required></textarea>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary" name="btnSubmit">Add</button>
+                                </div>
+                                </form>
                             </div>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-
     </div>
     <!-- /.container -->
 <?php 
--- timesheet/user.php
+++ timesheet/user.php
@@ -1,7 +1,44 @@
 <?php
-    include("top.php");
-    //include("header.php"); 
-    include("nav.php");
+include("top.php");
+//include("header.php"); 
+include("nav.php");
+include("lib/functions.php");
+
+if(!isset($_SESSION['userID'])) {
+    header('location: login.php');
+    exit();
+}
+
+if (isset($_POST["btnSubmit"])) {
+    $dataRecord = array();
+
+    $password = sha1(time());
+    $dataRecord[] = $password;
+
+    $email = filter_var($_POST["txtEmail"], FILTER_SANITIZE_EMAIL);
+    $dataRecord[] = $email;
+
+    $firstName = htmlentities($_POST["txtFirstName"], ENT_QUOTES, "UTF-8");
+    $dataRecord[] = $firstName;
+
+    $lastName = htmlentities($_POST["txtLastName"], ENT_QUOTES, "UTF-8");
+    $dataRecord[] = $lastName;
+
+    if ($email == "") {
+        $emailERROR = true;
+        alert_danger("You were not able to add a user.");
+    } else {
+        $query = 'INSERT INTO tblUser SET fldPassword = ?, fldEmail = ?, fldFirstName = ?, fldLastName = ?';
+
+        $results = $thisDatabase->insert($query, $dataRecord);
+
+        if($results == true) {
+            alert_success("You've successfully add a user.");
+        } else {
+            alert_danger("You were not able to add a user.");
+        }
+    }
+}
 ?>
     
     <div class="container">
@@ -10,65 +47,58 @@
             <div class="box">
                 <div class="col-lg-12">
                     <hr>
-                    <h2 class="intro-text text-center">Add User</h2>
+                    <h2 class="intro-text text-center"><strong>User</strong></h2>
                     <hr>
-                    <form role="form">
-                        <div class="row">
-                            <div class="form-group col-md-4">
-                                <label>First Name</label>
-                                <input type="text" class="form-control">
+                    <div class="row">
+                        <div class="form-group col-lg-4">
+                            <div class="form-group col-lg-4">
+                                <!-- Button trigger modal -->
+                                <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                                    Add User
+                                </button>
                             </div>
-                            <div class="form-group col-md-4">
-                                <label>Last Name</label>
-                                <input type="text" class="form-control">
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label>Email Address</label>
-                                <input type="email" class="form-control">
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label>Type</label>
-                                <input type="text" class="form-control">
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label>Position</label>
-                                <input type="text" class="form-control">
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label>Admission Date</label>
-                                <input type="text" class="form-control">
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label>Phone Number</label>
-                                <input type="tel" class="form-control">
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label>Adress</label>
-                                <input type="text" class="form-control">
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label>State</label>
-                                <input type="text" class="form-control">
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label>Zip Code</label>
-                                <input type="text" class="form-control">
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label>Country</label>
-                                <input type="text" class="form-control">
-                            </div>
-                            <div class="clearfix"></div>
-                            <div class="form-group col-lg-12">
-                                <input type="hidden" name="save" value="contact">
-                                <button type="submit" class="btn btn-default">Add</button>
+                            <div class="row">
+                                <ul class="list-group">
+                                    <?php build_list_from_database($thisDatabase, 'SELECT fldFirstName FROM tblUser ORDER BY fldFirstName');?>
+                                </ul>
                             </div>
                         </div>
-                    </form>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                                        <h4 class="modal-title" id="exampleModalLabel">Add User</h4>
+                                    </div>
+                                    <form role="form" method="post">
+                                    <div class="modal-body">
+                                        <div class="form-group">
+                                            <label>Email*</label>
+                                            <input type="email" name="txtEmail" class="form-control" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>First Name*</label>
+                                            <input type="text" name="txtFirstName" class="form-control" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Last Name*</label>
+                                            <input type="text" name="txtLastName" class="form-control" required>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                        <button type="submit" class="btn btn-primary" name="btnSubmit">Add</button>
+                                    </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-
     </div>
     <!-- /.container -->
 <?php 
