fbfede1e1e06597a23153ec2c381248969ea8f07
CarlosRodrigo <carlosrodrigocrcg@gmail.com>
2014-11-19 22:26:17 -0500

    Fixing select tag from database function

--- timesheet/lib/functions.php
+++ timesheet/lib/functions.php
@@ -3,8 +3,31 @@ function build_select_from_database($thisDatabase, $query) {
     $results = $thisDatabase->select($query);
 
     foreach ($results as $row) {
-        $data = $row[0];
-        print '<option value="' . $data . '">' . $data . '</option>';
+        $id = $row[0];
+        $data = $row[1];
+        print '<option value="' . $id . '">' . $data . '</option>';
     }
 }
+
+function build_list_from_database($thisDatabase, $query) {
+	$results = $thisDatabase->select($query);
+
+	foreach ($results as $row) {
+        $data = $row[0];
+        print '<li class="list-group-item">' . $data . '</li>';
+    }
+}
+
+function alert_success($message) {
+    print "<div class='alert alert-success' role='alert'><button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button>"
+        . $message . "
+        </div>";
+}
+
+function alert_danger($message) {
+    print "<div class='alert alert-danger' role='alert'><button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button>"
+        . $message . "
+        </div>";
+}
+
 ?>
