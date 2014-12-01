97ecd27eb34c2cf339e6d308e260715268325bf9
colin luther <cluther@uvm.edu>
2014-12-01 09:38:35 -0500

    Removing Latin and adding formatting
    Added formatting to code on contact.php
    
    Replaced Latin text on contact.php

--- timesheet/contact.php
+++ timesheet/contact.php
@@ -1,57 +1,55 @@
 <?php
-    include("top.php");
-    //include("header.php"); 
-    include("nav.php");
+include("top.php");
+//include("header.php"); 
+include("nav.php");
 
-    if(!isset($_SESSION['userID'])) {
-        header('location: login.php');
-        exit();
-    }
+if (!isset($_SESSION['userID'])) {
+    header('location: login.php');
+    exit();
+}
 ?>
 
-    <div class="container">
+<div class="container">
 
-        <div class="row">
-            <div class="box">
-                <div class="col-lg-12">
-                    <hr>
-                    <h2 class="intro-text text-center">Contact
-                        <strong>form</strong>
-                    </h2>
-                    <hr>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, vitae, distinctio, possimus repudiandae cupiditate ipsum excepturi dicta neque eaque voluptates tempora veniam esse earum sapiente optio deleniti consequuntur eos voluptatem.</p>
-                    <form role="form">
-                        <div class="row">
-                            <div class="form-group col-lg-4">
-                                <label>Name</label>
-                                <input type="text" class="form-control">
-                            </div>
-                            <div class="form-group col-lg-4">
-                                <label>Email Address</label>
-                                <input type="email" class="form-control">
-                            </div>
-                            <div class="form-group col-lg-4">
-                                <label>Phone Number</label>
-                                <input type="tel" class="form-control">
-                            </div>
-                            <div class="clearfix"></div>
-                            <div class="form-group col-lg-12">
-                                <label>Message</label>
-                                <textarea class="form-control" rows="6"></textarea>
-                            </div>
-                            <div class="form-group col-lg-12">
-                                <input type="hidden" name="save" value="contact">
-                                <button type="submit" class="btn btn-default">Submit</button>
-                            </div>
+    <div class="row">
+        <div class="box">
+            <div class="col-lg-12">
+                <hr>
+                <h2 class="intro-text text-center">Contact form</h2>
+                <hr>
+                <p>Please fill out the form accurately and press submit. We will contact you with any changes to projects you are involved in.</p>
+                <form role="form">
+                    <div class="row">
+                        <div class="form-group col-lg-4">
+                            <label>Name</label>
+                            <input type="text" class="form-control">
                         </div>
-                    </form>
-                </div>
+                        <div class="form-group col-lg-4">
+                            <label>Email Address</label>
+                            <input type="email" class="form-control">
+                        </div>
+                        <div class="form-group col-lg-4">
+                            <label>Phone Number</label>
+                            <input type="tel" class="form-control">
+                        </div>
+                        <div class="clearfix"></div>
+                        <div class="form-group col-lg-12">
+                            <label>Message</label>
+                            <textarea class="form-control" rows="6"></textarea>
+                        </div>
+                        <div class="form-group col-lg-12">
+                            <input type="hidden" name="save" value="contact">
+                            <button type="submit" class="btn btn-default">Submit</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
-
     </div>
-    <!-- /.container -->
 
-<?php 
-    include('footer.php');
+</div>
+<!-- /.container -->
+
+<?php
+include('footer.php');
 ?>
